95a7138c6b1e2bf8361388adac62985123cffc73
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-04 23:45:10 +0000

    mandatory tasks completed

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newValue = []
+  for (const value of array) {
+    newValue.push(appendString + value);
+  }
+
+  return array;
+}
