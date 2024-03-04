3c5c4c4dfc8f433cc48113c77e7d638f47f59cb9
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-04 22:54:01 +0000

    modified block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,8 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
-  const task2 = true;
+  let task2 = true;
 
   if (trueOrFalse) {
-    let task = false;
-    const task2 = true;
-    return [task, task2];
   }
 
   return [task, task2];
