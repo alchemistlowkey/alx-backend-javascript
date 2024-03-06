ba998868ad01346aea39d0667a2603b480843f99
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-06 16:54:32 +0000

    Promise returned

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+      const responseData = true;
+      resolve(responseData);
+  });
+}
+
+export default getResponseFromAPI;
