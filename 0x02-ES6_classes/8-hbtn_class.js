ac23a91419308194a938d3ecc60811f5ff97ea88
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-08 04:30:46 +0000

    code fixed for task 9

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,19 @@
+class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number' || typeof location !== 'string') throw new Error();
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](cast) {
+    if (cast === 'number') {
+      return this._size;
+    }
+    if (cast === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
+
+export default HolbertonClass;
